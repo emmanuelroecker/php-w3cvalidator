e928ce2ace3b5f6053afb68bc93f907b81ce9502
Emmanuel ROECKER <emmanuel.roecker@glicer.com>
2015-12-27 16:08:33 +0100

    travis ci test

---
+++ test.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * {SHORT_DESCRIPTION}
+ *
+ * PHP version 5.4
+ *
+ * @category  GLICER
+ * @package   Contact
+ * @author    Emmanuel ROECKER <emmanuel.roecker@gmail.com>
+ * @author    Rym BOUCHAGOUR <rym.bouchagour@free.fr>
+ * @copyright 2012-2013 GLICER
+ * @license   Proprietary property of GLICER
+ * @link      http://www.glicer.com
+ *
+ * Created : 27/12/15
+ * File : test.php
+ *
+ */
+
+$test = "bonjour";
+
+var_dump($test);
